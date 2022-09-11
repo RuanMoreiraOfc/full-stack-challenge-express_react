7f0a7d2db669b7d48de3edade876b9317bf218bb
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 18:29:07 -0300

    feat: create `ChangeFavoriteUserWordController` controller

---
+++ backend/src/controllers/entries/en/favorite/ChangeFavoriteUserWordController.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from 'express';
+
+import verifyToken from '@utils/verifyToken';
+
+import type { IChangeFavoriteUserWordUseCase } from '@entries/useCases/IChangeFavoriteUserWordUseCase';
+
+export { ChangeFavoriteUserWordController };
+
+class ChangeFavoriteUserWordController {
+  constructor(private useCase: IChangeFavoriteUserWordUseCase) {}
+
+  async handle(request: Request, response: Response) {
+    const { word } = request.params;
+    const state = request.method === 'DELETE';
+    const user_id = verifyToken(request);
+
+    await this.useCase.execute({
+      state,
+      word,
+      user_id,
+    });
+
+    response.status(204).end();
+  }
+}
