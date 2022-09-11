d152947b5d368375a5ecbce8a2f6b1fd2d0eb82c
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 15:34:33 -0300

    feat: create `WordRepository` repository

---
+++ backend/src/entries/repositories/implementations/WordRepository.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+
+import { Word } from '@entries/models/Word';
+import type {
+  IWordPaginationDTO,
+  IWordRepository,
+} from '@entries/repositories/IWordRepository';
+
+export { WordRepository };
+
+class WordRepository implements IWordRepository {
+  private prisma: PrismaClient;
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async list({ search, limit = 20 }: IWordPaginationDTO) {
+    const wordsFromDb = await this.prisma.word.findMany({
+      take: limit,
+      orderBy: {
+        value: 'asc',
+      },
+      where: {
+        value: {
+          startsWith: search,
+        },
+      },
+    });
+
+    const words = wordsFromDb.map((word) => new Word(word));
+
+    return words;
+  }
+}
