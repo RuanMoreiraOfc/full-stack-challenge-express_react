c7f89933f07a4e33b0ad5b8d4c2b95ba4b198d5a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 15:34:21 -0300

    feat: create `IWordRepository` interface

---
+++ backend/src/entries/repositories/IWordRepository.d.ts
@@ -0,0 +1,12 @@
+import type { IWord } from '@entries/models/Word';
+
+export type { IWordPaginationDTO, IWordRepository };
+
+interface IWordPaginationDTO {
+  search?: string;
+  limit?: number;
+}
+
+interface IWordRepository {
+  list(data: IWordPaginationDTO): Promise<IWord[]>;
+}
