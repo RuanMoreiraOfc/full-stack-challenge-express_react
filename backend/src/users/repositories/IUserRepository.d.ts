5653bdc19bf3ac8dbe0702b8b32e7479d6672780
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 10:52:05 -0300

    feat(IUserRepository): add `create` method

--- backend/src/users/repositories/IUserRepository.d.ts
+++ backend/src/users/repositories/IUserRepository.d.ts
@@ -1,7 +1,14 @@
 import { IUser } from '@users/models/User';
 
-export type { IUserRepository };
+export type { ICreateUserDTO, IUserRepository };
+
+interface ICreateUserDTO {
+  name: string;
+  email: string;
+  password: string;
+}
 
 interface IUserRepository {
+  create(data: ICreateUserDTO): Promise<IUser>;
   findByEmail(email: string): Promise<IUser | null>;
 }
--- backend/src/users/repositories/implementations/UserRepository.ts
+++ backend/src/users/repositories/implementations/UserRepository.ts
@@ -2,7 +2,10 @@ import { hash } from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 
 import { User } from '@users/models/User';
-import { IUserRepository } from '@users/repositories/IUserRepository';
+import {
+  ICreateUserDTO,
+  IUserRepository,
+} from '@users/repositories/IUserRepository';
 
 export { UserRepository };
 
@@ -10,6 +13,22 @@ class UserRepository implements IUserRepository {
   private prisma: PrismaClient;
   constructor() {
     this.prisma = new PrismaClient();
+  }
+
+  async create({ name, email, password }: ICreateUserDTO) {
+    const password_hash = await hash(password, 10);
+
+    const userOnDB = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password_hash,
+      },
+    });
+
+    const user = new User(userOnDB);
+
+    return user;
   }
 
   async findByEmail(email: string) {
