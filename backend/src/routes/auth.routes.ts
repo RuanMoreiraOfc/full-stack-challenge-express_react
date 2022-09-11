a0ab3524405607747cbf9dfd6d22b97d79b19477
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 12:45:08 -0300

    feat: create  `/auth/signin` route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { SignUpController } from '@controllers/auth/signup';
+import { SignInController } from '@controllers/auth/signin';
 
 export { routes as authRoutes };
 
 const routes = Router();
 
 routes.post('/signup', (req, res) => SignUpController.handle(req, res));
+routes.post('/signin', (req, res) => SignInController.handle(req, res));
