ce2b4c9ef486ee56f24884c7c6ad0a996b17078a
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 14:01:06 -0300

    feat: setup swagger route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,15 @@
+import swaggerDocument from '@public/swagger.json';
+
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { authRoutes } from '@routes/auth.routes';
 
 export { routes as allRoutes };
 
 const routes = Router();
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 routes.use('/auth', authRoutes);
