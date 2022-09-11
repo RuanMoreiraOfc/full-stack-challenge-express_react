a7b8692e855e5171441230c86eaeb85d6c6eb1db
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-11 18:31:16 -0300

    feat: "/entries/en/:word/favorite" & "/entries/en/:word/unfavorite" routes

--- backend/src/routes/entries.routes.ts
+++ backend/src/routes/entries.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
 import { ListWordsController } from '@controllers/entries/en';
+import { ChangeFavoriteUserWordController } from '@controllers/entries/en/favorite';
 
 export { routes as entriesRoutes };
 
 const routes = Router();
 
 routes.get('/en', (req, res) => ListWordsController.handle(req, res));
+routes.post('/en/:word/favorite', (req, res) =>
+  ChangeFavoriteUserWordController.handle(req, res),
+);
+routes.delete('/en/:word/unfavorite', (req, res) =>
+  ChangeFavoriteUserWordController.handle(req, res),
+);
