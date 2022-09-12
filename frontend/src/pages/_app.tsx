1403289025d6a01515daf48a57b393edb86cad89
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-12 19:19:48 -0300

    feat(frontend): create `/signin` & `/signup` routes

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import SignIn from '@pages/signin';
+import SignUp from '@pages/signup';
+
 export default App;
 
 function App() {
-   return <>Fullstack Challenge 2022</>;
+   return (
+      <Router>
+         <Routes>
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signup' element={<SignUp />} />
+         </Routes>
+      </Router>
+   );
 }
