c08e31fccd6c3fe65a0bba93029505aa91306f65
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-12 08:47:48 -0300

    feat: create `ListFavoriteUsersWordsUseCase` use case

---
+++ backend/src/entries/useCases/implementations/ListFavoriteUsersWordsUseCase.ts
@@ -0,0 +1,50 @@
+import type {
+  IUserWordPaginationDTO,
+  IUserWordRepository,
+} from '@entries/repositories/IUserWordRepository';
+import type {
+  IListFavoriteUsersWordsUseCase,
+  IListFavoriteUsersWordsUseCaseResponse,
+} from '@entries/useCases/IListFavoriteUsersWordsUseCase';
+
+export { ListFavoriteUsersWordsUseCase };
+
+class ListFavoriteUsersWordsUseCase implements IListFavoriteUsersWordsUseCase {
+  constructor(private repository: IUserWordRepository) {}
+
+  async execute({
+    user_id,
+    favorite,
+    viewed,
+    limit,
+    page,
+  }: IUserWordPaginationDTO): Promise<IListFavoriteUsersWordsUseCaseResponse> {
+    const totalDocs = await this.repository.count({
+      user_id,
+      favorite,
+      viewed,
+    });
+    const totalPages = Math.ceil(totalDocs / limit);
+    const hasNext = page < Math.ceil(totalDocs / limit);
+    const hasPrev = page > 1;
+
+    // ***
+
+    const results = await this.repository.list({
+      user_id,
+      favorite,
+      viewed,
+      page,
+      limit,
+    });
+
+    return {
+      results,
+      totalDocs,
+      page: Number(page),
+      totalPages,
+      hasNext,
+      hasPrev,
+    };
+  }
+}
