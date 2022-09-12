6e0c1cf479b025de4884feed8fddb81f4a48589e
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-12 19:41:37 -0300

    feat(frontend): use `EnsureAuthenticated` middleware

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import EnsureAuthenticated from '@middlewares/EnsureAuthenticated';
 import SignIn from '@pages/signin';
 import SignUp from '@pages/signup';
 
@@ -8,6 +9,7 @@ export default App;
 function App() {
    return (
       <Router>
+         <EnsureAuthenticated />
          <Routes>
             <Route path='/signin' element={<SignIn />} />
             <Route path='/signup' element={<SignUp />} />
