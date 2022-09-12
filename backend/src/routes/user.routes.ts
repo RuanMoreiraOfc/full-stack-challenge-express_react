4c22c52b416af465de2dbb6eae32447bf68d5111
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-12 10:58:04 -0300

    feat: create "/user/me/history" route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { ListViewedUsersWordsController } from '@controllers/user/me/history';
 import { ListFavoriteUsersWordsController } from '@controllers/user/me/favorites';
 
 export { routes as userRoutes };
 
 const routes = Router();
 
+routes.get('/history', (req, res) =>
+  ListViewedUsersWordsController.handle(req, res),
+);
 routes.get('/favorites', (req, res) =>
   ListFavoriteUsersWordsController.handle(req, res),
 );
