efbc1377c50fe4e2ae5efe19960ff0598f34968c
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-12 10:48:43 -0300

    refactor(ensureAuthenticatedMiddleware): remove redundant code

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -1,30 +1,15 @@
 import type { Request, Response, NextFunction } from 'express';
 
-import { PrismaClientSingleton } from '@utils/PrismaClientSingleton';
 import verifyToken from '@utils/verifyToken';
 
-import { AppError } from '@errors/AppError';
-
 export { ensureAuthenticatedMiddleware };
-
-const prisma = PrismaClientSingleton.getInstance().client;
 
 async function ensureAuthenticatedMiddleware(
   request: Request,
   response: Response,
   next: NextFunction,
 ) {
-  const id = await verifyToken(request);
-
-  const user = prisma.user.findFirst({
-    where: {
-      id,
-    },
-  });
-
-  if (!user) {
-    throw new AppError({ message: 'Invalid token!', statusCode: 401 });
-  }
+  await verifyToken(request);
 
   next();
 }
