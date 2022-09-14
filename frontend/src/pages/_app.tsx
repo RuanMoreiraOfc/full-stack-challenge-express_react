fc0173a2f585ab875c8c1f7a5acc73c199d9cd21
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-09-14 06:19:48 -0300

    feat(fronted): add navigation urls in homepage

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { Flex, Grid, Link as StyledLink } from '@chakra-ui/react';
 
 import EnsureAuthenticated from '@middlewares/EnsureAuthenticated';
 import SignIn from '@pages/signin';
@@ -11,6 +13,22 @@ function App() {
       <Router>
          <EnsureAuthenticated>
             <Routes>
+               <Route
+                  path='/'
+                  element={
+                     <Grid w='full' h='100vh' placeItems='center'>
+                        Fullstack Challenge 2022
+                        <Flex flexWrap='wrap' gap='8' color='blue'>
+                           <StyledLink as={Link} to='/signin'>
+                              Login in your account
+                           </StyledLink>
+                           <StyledLink as={Link} to='/signup'>
+                              Create an account
+                           </StyledLink>
+                        </Flex>
+                     </Grid>
+                  }
+               />
                <Route path='/signin' element={<SignIn />} />
                <Route path='/signup' element={<SignUp />} />
             </Routes>
